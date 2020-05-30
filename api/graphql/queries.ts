4e46211ef44cd6b4d4e4177ff4a4325cff1e02fe
Carter Rabasa <carter.rabasa@gmail.com>
2020-05-30 07:56:08 -0700

    added openGraph queries + formatting

--- api/graphql/queries.ts
+++ api/graphql/queries.ts
@@ -1,24 +1,26 @@
-import { schema } from "nexus";
+import { schema } from 'nexus'
 
 schema.queryType({
-    definition(t) {
-      t.crud.activity()
-      t.crud.activities({pagination: true, filtering: true})
-      t.crud.experience()
-      t.crud.experiences({pagination: true, filtering: true})
-      t.crud.place()
-      t.crud.places({pagination: true, filtering: true})
-      t.crud.preference()
-      t.crud.preferences({pagination: true, filtering: true})
-      t.crud.profile()
-      t.crud.profiles({pagination: true, filtering: true})
-      t.crud.skill()
-      t.crud.skills({pagination: true, filtering: true})
-      t.crud.tool()
-      t.crud.tools({pagination: true, filtering: true})
-      t.crud.trait()
-      t.crud.traits({pagination: true, filtering: true})
-      t.crud.user()
-      t.crud.users({pagination: true, filtering: true})
-    }
-  })
+  definition(t) {
+    t.crud.activity()
+    t.crud.activities({ pagination: true, filtering: true })
+    t.crud.experience()
+    t.crud.experiences({ pagination: true, filtering: true })
+    t.crud.place()
+    t.crud.places({ pagination: true, filtering: true })
+    t.crud.openGraph()
+    t.crud.openGraphs({ pagination: true, filtering: true })
+    t.crud.preference()
+    t.crud.preferences({ pagination: true, filtering: true })
+    t.crud.profile()
+    t.crud.profiles({ pagination: true, filtering: true })
+    t.crud.skill()
+    t.crud.skills({ pagination: true, filtering: true })
+    t.crud.tool()
+    t.crud.tools({ pagination: true, filtering: true })
+    t.crud.trait()
+    t.crud.traits({ pagination: true, filtering: true })
+    t.crud.user()
+    t.crud.users({ pagination: true, filtering: true })
+  },
+})
