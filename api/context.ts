84a615d800715a6a128250b9c6b437ec87956121
Carter Rabasa <carter.rabasa@gmail.com>
2020-06-01 18:06:50 -0700

    custom context type to fix TS errors

---
+++ api/context.ts
@@ -0,0 +1,19 @@
+const ROLES_KEY = 'https://fizbuz.com/roles'
+const EMAIL_KEY = 'https://fizbuz.com/email'
+
+interface Auth0User {
+  sub: string
+  'https://fizbuz.com/roles': string[]
+  iss: string
+  aud: string[]
+  iat: number
+  exp: number
+  azp: string
+  scope: string
+}
+
+interface CustomContext extends NexusContext {
+  user: Auth0User
+}
+
+export { Auth0User, CustomContext, ROLES_KEY, EMAIL_KEY }
