48cd72d4a0ac6be2a53859f37df4bf6cece7ba36
Carter Rabasa <carter.rabasa@gmail.com>
2020-06-01 17:57:14 -0700

    moved auth0sub value to Identities table

--- api/graphql/models.ts
+++ api/graphql/models.ts
@@ -1,7 +1,7 @@
-import { schema } from "nexus"
+import { schema } from 'nexus'
 
 schema.objectType({
-  name: "OpenGraph",
+  name: 'OpenGraph',
   definition(t) {
     t.model.id()
     t.model.description()
@@ -11,29 +11,41 @@ schema.objectType({
     t.model.urlhash()
     t.model.createdAt()
     t.model.updatedAt()
-    t.model.deletedAt()    
-  }
+    t.model.deletedAt()
+  },
 })
 
 schema.objectType({
-  name: "User",
+  name: 'User',
   definition(t) {
     t.model.id()
-    t.model.auth0Sub()
     t.model.email()
     t.model.productEmail()
     t.model.promotionalEmail()
     t.model.inboundConnections()
     t.model.outboundConnections()
+    t.model.identities()
     t.model.profile()
     t.model.createdAt()
     t.model.updatedAt()
     t.model.deletedAt()
-  }
+  },
 })
 
 schema.objectType({
-  name: "Trait",
+  name: 'Identity',
+  definition(t) {
+    t.model.id()
+    t.model.user()
+    t.model.auth0Sub()
+    t.model.createdAt()
+    t.model.updatedAt()
+    t.model.deletedAt()
+  },
+})
+
+schema.objectType({
+  name: 'Trait',
   definition(t) {
     t.model.id()
     t.model.description()
@@ -43,12 +55,12 @@ schema.objectType({
     t.model.verified()
     t.model.createdAt()
     t.model.updatedAt()
-    t.model.deletedAt()    
-  }
+    t.model.deletedAt()
+  },
 })
 
 schema.objectType({
-  name: "Tool",
+  name: 'Tool',
   definition(t) {
     t.model.id()
     t.model.description()
@@ -59,12 +71,12 @@ schema.objectType({
     t.model.verified()
     t.model.createdAt()
     t.model.updatedAt()
-    t.model.deletedAt()    
-  }
+    t.model.deletedAt()
+  },
 })
 
 schema.objectType({
-  name: "Place",
+  name: 'Place',
   definition(t) {
     t.model.id()
     t.model.description()
@@ -73,12 +85,12 @@ schema.objectType({
     t.model.kind()
     t.model.createdAt()
     t.model.updatedAt()
-    t.model.deletedAt()    
-  }
+    t.model.deletedAt()
+  },
 })
 
 schema.objectType({
-  name: "Connection",
+  name: 'Connection',
   definition(t) {
     t.model.id()
     t.model.connected()
@@ -87,11 +99,11 @@ schema.objectType({
     t.model.createdAt()
     t.model.updatedAt()
     t.model.deletedAt()
-  }
+  },
 })
 
 schema.objectType({
-  name: "Profile",
+  name: 'Profile',
   definition(t) {
     t.model.id()
     t.model.location()
@@ -107,11 +119,11 @@ schema.objectType({
     t.model.createdAt()
     t.model.updatedAt()
     t.model.deletedAt()
-  }
+  },
 })
-      
+
 schema.objectType({
-  name: "Preference",
+  name: 'Preference',
   definition(t) {
     t.model.id()
     t.model.comment()
@@ -120,12 +132,12 @@ schema.objectType({
     t.model.trait()
     t.model.createdAt()
     t.model.updatedAt()
-    t.model.deletedAt()    
-  }
+    t.model.deletedAt()
+  },
 })
 
 schema.objectType({
-  name: "Skill",
+  name: 'Skill',
   definition(t) {
     t.model.id()
     t.model.comment()
@@ -135,12 +147,12 @@ schema.objectType({
     t.model.tool()
     t.model.createdAt()
     t.model.updatedAt()
-    t.model.deletedAt()    
-  }
+    t.model.deletedAt()
+  },
 })
 
 schema.objectType({
-  name: "Activity",
+  name: 'Activity',
   definition(t) {
     t.model.id()
     t.model.description()
@@ -152,12 +164,12 @@ schema.objectType({
     t.model.url()
     t.model.createdAt()
     t.model.updatedAt()
-    t.model.deletedAt()    
-  }
+    t.model.deletedAt()
+  },
 })
 
 schema.objectType({
-  name: "External",
+  name: 'External',
   definition(t) {
     t.model.id()
     t.model.profile()
@@ -165,12 +177,12 @@ schema.objectType({
     t.model.service()
     t.model.createdAt()
     t.model.updatedAt()
-    t.model.deletedAt()    
-  }
+    t.model.deletedAt()
+  },
 })
 
 schema.objectType({
-  name: "Experience",
+  name: 'Experience',
   definition(t) {
     t.model.id()
     t.model.kind()
@@ -182,7 +194,6 @@ schema.objectType({
     t.model.place()
     t.model.createdAt()
     t.model.updatedAt()
-    t.model.deletedAt()    
-  }
+    t.model.deletedAt()
+  },
 })
-
