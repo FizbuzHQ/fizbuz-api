494563f7d02b0f8160456b215a4471bf62a644c1
Carter Rabasa <carter.rabasa@gmail.com>
2020-06-01 18:05:28 -0700

    lint fixes

--- api/app.ts
+++ api/app.ts
@@ -1,8 +1,7 @@
 import cors from 'cors'
-import { settings, server, use } from 'nexus'
+import { settings, server, use, schema } from 'nexus'
 import { prisma } from 'nexus-plugin-prisma'
 import authenticated from './security'
-import { schema } from 'nexus'
 
 // Prisma is our connection to the underlying database
 use(prisma())
@@ -12,17 +11,17 @@ server.express.use(cors())
 
 // Authenticates requests using Auth0
 server.express.use((req: any, res, next) => {
-    authenticated(req, res, () => {
-        let { user } = req
-        schema.addToContext(() => {
-            return { user };
-        });
-        next()
+  authenticated(req, res, () => {
+    const { user } = req
+    schema.addToContext(() => {
+      return { user }
     })
+    next()
+  })
 })
 
 settings.change({
-    server: {
-        playground: true,
-    }
+  server: {
+    playground: true,
+  },
 })
--- api/security.ts
+++ api/security.ts
@@ -1,20 +1,20 @@
-const jwt = require('express-jwt');
-const jwksRsa = require('jwks-rsa');
+import jwt from 'express-jwt'
+import jwksRsa from 'jwks-rsa'
 
 // Auth0 configuration
 const authConfig = {
   issuer: 'https://fizbuz.auth0.com/',
-  audience: 'https://alpha.fizbuz.com',
+  audience: 'https://api.fizbuz.com',
   algorithm: ['RS256'],
-};
+}
 
 const secret = jwksRsa.expressJwtSecret({
   cache: true,
   rateLimit: true,
   jwksRequestsPerMinute: 5,
   jwksUri: `${authConfig.issuer}.well-known/jwks.json`,
-});
+})
 
-const authenticated = jwt({ secret, ...authConfig });
+const authenticated = jwt({ secret, ...authConfig })
 
-export default authenticated;
+export default authenticated
