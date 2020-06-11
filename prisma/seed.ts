0707926237d5431e8654f92ea1a8aa8a87e1af7a
Carter Rabasa <carter.rabasa@gmail.com>
2020-06-11 13:47:23 -0700

    data load for Tools

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,24 +1,37 @@
 import { PrismaClient, ToolKind } from '@prisma/client'
+import tools from './data/tools.json'
 
 const db = new PrismaClient()
 
-main()
+async function loadTools() {
+  for (const i in tools) {
+    const record = tools[i]
+    //console.log(record)
+    // transform the exported data
+    const tool = {
+      data: {
+        name: record.name,
+        nameSearch: record.nameSEARCH,
+        kind: record.kind as ToolKind, // needed to deal with enum value
+        description: record.description,
+        url: record.url,
+        verified: record.verified,
+        createdAt: record.created ? new Date(parseInt(record.created)) : null,
+        updatedAt: record.updated ? new Date(parseInt(record.updated)) : null,
+      },
+    }
+    try {
+      const saved = await db.tool.create(tool)
+      console.log(`Record imported: ${saved.id} ${saved.name}`)
+    } catch (err) {
+      console.log('Record failed to import: ', record, err)
+    }
+  }
+}
 
 async function main() {
-  const results = await Promise.all(
-    [
-      {
-        name: 'JavaScript',
-        kind: 'Language' as ToolKind,
-      },
-      {
-        name: 'Ruby',
-        kind: 'Language' as ToolKind,
-      },
-    ].map((data) => db.tool.create({ data })),
-  )
-
-  console.log('Seeded: %j', results)
-
+  await loadTools()
   db.disconnect()
 }
+
+main()
